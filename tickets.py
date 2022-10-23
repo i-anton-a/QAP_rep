e991f373f5b6f70c72a1501b601ac11cc1afcab3
i-anton-a <66274142+i-anton-a@users.noreply.github.com>
2022-10-23 10:41:36 +0300

    Add files via upload

---
+++ tickets.py
@@ -0,0 +1,18 @@
+ticket = int(input('Сколько билетов будете брать?'))
+age = [int(input('Сколько вам лет ')) for i in range(ticket)]
+sum_total = 0
+sum1 = 990
+sum2 = 1390
+sum_ticket1 = 0
+sum_ticket2 = 0
+
+for i in range(len(age)):
+    if 18 <= age [i] <= 25:
+        sum_ticket1 += 1
+    if age [i] > 25:
+        sum_ticket2 += 1
+
+if ticket > 3:
+    sum_total = (sum1 * sum_ticket1 + sum2 * sum_ticket2) - (sum1 * sum_ticket1 + sum2 * sum_ticket2)*0.1
+
+print("Количество билетов : ", ticket, "По цене : ", sum_total)
